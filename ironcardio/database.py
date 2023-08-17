f598853a4aebe6010b08dd2e92a8179b03db8e96
Russell Helmstedter <rhelmstedter@gmail.com>
2023-08-16 19:13:53 -0700

    stuck; created new branch;

---
+++ ironcardio/database.py
@@ -0,0 +1,52 @@
+import configparser
+from pathlib import Path
+
+import json
+from typing import NamedTuple
+from ironcardio import DB_WRITE_ERROR, DB_READ_ERROR, JSON_ERROR, SUCCESS
+
+DEFAULT_DB_FILE_PATH = Path.home().joinpath("." + Path.home().stem + "_ironcardio.json")
+
+
+def get_database_path(config_file: Path) -> Path:
+    """Return the current path to the iron cardio database."""
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    return Path(config_parser["General"]["database"])
+
+
+def init_database(db_path: Path) -> int:
+    """Create the iron cardio database."""
+    try:
+        db_path.write_text("[]")
+        return SUCCESS
+    except OSError:
+        return DB_WRITE_ERROR
+
+
+class DBResponse(NamedTuple):
+    session_list: list[dict[str]]
+    error: int
+
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+
+    def read_sessions(self) -> DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError:  # Catch wrong JSON format
+                    return DBResponse([], JSON_ERROR)
+        except OSError:  # Catch file IO problems
+            return DBResponse([], DB_READ_ERROR)
+
+    def write_sessions(self, session_list: list[dict[str]]) -> DBResponse:
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(session_list, db, indent=4)
+            return DBResponse(session_list, SUCCESS)
+        except OSError:  # Catch file IO problems
+            return DBResponse(session_list, DB_WRITE_ERROR)
---
+++ ironcardio/ironcardio.py
@@ -0,0 +1,146 @@
+from collections import Counter
+from random import choices, choice
+from rich import print
+
+from typing import NamedTuple
+from pathlib import Path
+from ironcardio import DB_READ_ERROR
+from ironcardio.database import DatabaseHandler
+
+
+class Session(NamedTuple):
+    bells: str
+    variation: str
+    time: str
+    load: str
+    swings: str | int
+    error: int
+
+
+class Loads(NamedTuple):
+    units: str
+    light: int
+    medium: int
+    heavy: int
+    error: int
+
+
+BELLS = {"Single Bell": 4 / 6, "Double Bells": 2 / 6}
+DOUBLEBELL_VARIATIONS = {
+    "Double Classic": 2 / 6,
+    "Double Classic + Pullup": 1 / 6,
+    "Double Traveling 2s": 2 / 6,
+    "SFG II Focus": 1 / 6,
+}
+SINGLEBELL_VARIATIONS = {
+    "Classic": 1 / 6,
+    "Classic + Pullup": 1 / 6,
+    "Classic + Snatch": 1 / 6,
+    "Traveling 2s": 1 / 6,
+    "Traveling 2s + Snatch": 1 / 6,
+    "Traveling 2s + Pullup": 1 / 6,
+}
+TIMES = {
+    "30 mins": 1 / 6,
+    "20 mins": 4 / 6,
+    "10 mins": 1 / 6,
+}
+LOADS = {
+    "heavy load": 2 / 6,
+    "medium load": 3 / 6,
+    "light load": 1 / 6,
+}
+SWINGS = {"Yes": 2 / 6, "No Swings": 4 / 6}
+
+
+def set_loads():
+    """Set the weight for the light, medium, and heavy loads."""
+    units = input("[P]ounds or [K]ilograms.").lower()
+    if units.startswith() == "p":
+        units = "pounds"
+    else:
+        units = "kilograms"
+    light_load = int(input("Enter the weight for the light kettlebells:\n> "))
+    medium_load = int(input("Enter the weight for the medium kettlebells:\n> "))
+    heavy_load = int(input("Enter the weight for the heavy kettlebells:\n> "))
+    return Loads(units, light_load, medium_load, heavy_load)
+
+
+def create_ic_session():
+    bells = choices(
+        population=tuple(BELLS.keys()),
+        weights=tuple(BELLS.values()),
+    )[0]
+    if bells == "Double Bells":
+        variation = choices(
+            population=tuple(DOUBLEBELL_VARIATIONS.keys()),
+            weights=tuple(DOUBLEBELL_VARIATIONS.values()),
+        )[0]
+    elif bells == "Single Bell":
+        variation = choices(
+            population=tuple(SINGLEBELL_VARIATIONS.keys()),
+            weights=tuple(SINGLEBELL_VARIATIONS.values()),
+        )[0]
+    time = choices(
+        population=tuple(TIMES.keys()),
+        weights=tuple(TIMES.values()),
+    )[0]
+    load = choices(
+        population=tuple(LOADS.keys()),
+        weights=tuple(LOADS.values()),
+    )[0]
+    swings = choices(
+        population=tuple(SWINGS.keys()),
+        weights=tuple(SWINGS.values()),
+    )[0]
+    if swings == "yes":
+        swings = choice(range(50, 110, 10))
+    return Session(bells, variation, time, load, swings)
+
+
+def simulation():
+    sessions = [create_ic_session() for s in range(3 * 52)]
+    stats = Counter()
+    for session in sessions:
+        for c in session:
+            if isinstance(c, int):
+                c = "Swings"
+            stats.update([c])
+    one_year = dict(sorted(stats.items(), key=lambda x: x[1], reverse=True))
+    width = len(max(one_year.keys(), key=len))
+    for session, count in one_year.items():
+        print(f"{session: >{width}}: " + "#" * count)
+
+
+# class SessionConstructor:
+#     def __init__(self, db_path: Path) -> None:
+#         self._db_handler = DatabaseHandler(db_path)
+#
+#     def add(self, description: list[str], priority: int = 2) -> Session:
+#         """Add a new session to the database."""
+#         session = create_ic_session()
+#
+#         read = self._db_handler.read_sessions()
+#         if read.error == DB_READ_ERROR:
+#             return Session(session, read.error)
+#         read.todo_list.append(session)
+#         write = self._db_handler.write_todos(read.todo_list)
+#         return Session(session, write.error)
+#
+
+if __name__ == "__main__":
+    session = create_ic_session()
+    if session.swings == "yes":
+        swings = f"Swings: {session.swings} reps"
+    else:
+        swings = ""
+    print(
+        f"""Today's Workout
+    ===============
+    Bells: {session.bells.title()}
+    Variation: {session.variation}
+    Time: {session.time}
+    Load: {session.load.title()}
+    {swings}
+    """
+    )
