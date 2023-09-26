25a9c30851cdb9d4282772d46cf6e5dcd961b7b7
Russell Helmstedter <rhelmstedter@gmail.com>
2023-09-25 18:29:17 -0700

    removed SFG II Focus; adjusted weights for single bell variations

--- src/iron_cardio/constants.py
+++ src/iron_cardio/constants.py
@@ -7,19 +7,18 @@ IRON_CARDIO_HOME = Path().home() / ".iron-cardio"
 IRON_CARDIO_DB = IRON_CARDIO_HOME / "iron_cardio_db.json"
 BELLS = {"Single Bell": 4 / 6, "Double Bells": 2 / 6}
 DOUBLEBELL_VARIATIONS = {
-    "Double Classic": 2 / 6,
-    "Double Classic + Pullup": 1 / 6,
+    "Double Classic": 3 / 6,
     "Double Traveling 2s": 2 / 6,
-    "SFG II Focus": 1 / 6,
+    "Double Classic + Pullup": 1 / 6,
 }
 SINGLEBELL_VARIATIONS = {
-    "Classic": 1 / 7,
-    "Classic + Pullup": 1 / 7,
-    "Classic + Snatch": 1 / 7,
-    "Traveling 2s": 1 / 7,
-    "Traveling 2s + Snatch": 1 / 7,
-    "Traveling 2s + Pullup": 1 / 7,
-    "Single Bell Armor Building Complex": 1 / 7,
+    "Classic": 3 / 10,
+    "Classic + Pullup": 1 / 10,
+    "Classic + Snatch": 1 / 10,
+    "Traveling 2s": 1 / 10,
+    "Traveling 2s + Snatch": 1 / 10,
+    "Traveling 2s + Pullup": 1 / 10,
+    "Single Bell Armor Building Complex": 2 / 10,
 }
 TIMES = {
     30: 1 / 6,
@@ -38,7 +37,6 @@ REP_SCHEMES = {
     "Classic + Pullup": 3,
     "Double Classic": 3,
     "Double Classic + Pullup": 3,
-    "SFG II Focus": 3,
     "Classic + Snatch": 4,
     "Traveling 2s": 4,
     "Traveling 2s + Pullup": 4,
